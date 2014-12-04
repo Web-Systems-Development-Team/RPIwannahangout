8c57daee1d2ecc0fd10210e08bcad41aa1a91ec2
Max Llewellyn <max.llewellyn@gmail.com>
2014-12-04 12:22:21 -0500

    added .mp4 video too for browser compatability

--- explore.php
+++ explore.php
@@ -13,7 +13,11 @@
     <div id="explore">
 	<?php include 'basic_includes/navbar.php' //create menu ?> 
     <div id='step1'>
-        <video width="50%"controls src="sitetutorial.webm" type="video/webm">Your browser does not support the video tag.</video>
+        <video width="50%" controls>
+          <source src="/sitetutorial.webm" type="video/webm">
+          <source src="/sitetutorial.mp4" type="video/mp4">
+          Your browser does not support the video tag.
+        </video>
     <p><h3>Step 1: </h3> Login using RPI credentials to submit and search for events </p></div>
     <div id='step2'><h3>Step 2: </h3> Submit events by navigating to the "Create an Event" page and filling out the required information </div>
     <div id='step3'><h3>Step 3: </h3> Find Events by navigating to the "Find an Event" page and scrolling through event listings </div>
