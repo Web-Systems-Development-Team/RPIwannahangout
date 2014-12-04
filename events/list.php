20b92b2748d038bcdda1ff0680901d3c2ac0abcb
powatm <powatm@rpi.edu>
2014-12-04 11:46:56 -0500

    Changed backgrounds, added icons for lists

--- events/list.php
+++ events/list.php
@@ -8,33 +8,36 @@
 <html>
 <head>
 	<?php include_once '../basic_includes/sheets_and_scripts.php' ?>
+    <link href="//maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">
 	<title>Event List</title>
 </head>
 <body>
     <div id="list_page">
-	<?php include '../basic_includes/navbar.php' ?>
+    <?php include '../basic_includes/navbar.php' ?>
 	<div class="panel panel-default" id="list_events">
 		<table class="table table-hover">
 			<thead>
-				<th class="sort col-md-3" data-sort="title">Title <span class="glyphicon glyphicon-sort" aria-hidden="true"></span></th>
-				<th class="sort col-md-2" data-sort="date">Date <span class="glyphicon glyphicon-sort" aria-hidden="true"></span></th>
-				<th class="sort col-md-1" data-sort="start_time">Start Time <span class="glyphicon glyphicon-sort" aria-hidden="true"></span></th>
-				<th class="sort col-md-1" data-sort="end_time">End Time <span class="glyphicon glyphicon-sort" aria-hidden="true"></span></th>
-				<th class="sort col-md-3" data-sort="location">Location <span class="glyphicon glyphicon-sort" aria-hidden="true"></span></th>
-				<th class="sort col-md-1" data-sort="max_attendance">Max Attendance</th>
-				<th class="sort col-md-1" data-sort="open_spots">Open Spots <span class="glyphicon glyphicon-sort" aria-hidden="true"></span></th>
-				<th colspan="2">
-		          <input type="text" class="search" placeholder="Search" />
-		        </th>
+				<th class="sort col-md-4" data-sort="title">Title<i class="fa fa-sort"></i></th>
+					<th class="sort col-md-1" data-sort="date">Date<i class="fa fa-sort"></i></th>
+					<th class="sort col-md-1" data-sort="start_time">Start Time<i class="fa fa-sort"></i></th>
+					<th class="sort col-md-1" data-sort="end_time">End Time<i class="fa fa-sort"></i></i></th>
+					<th class="sort col-md-2" data-sort="location">Location<i class="fa fa-sort"></i></th>
+					<th class="sort col-md-1" data-sort="attending">Act<i class="fa fa-sort"></i></th>
+					<th class="sort col-md-1" data-sort="max_attendance">Cap<i class="fa fa-sort"></i></th>
+                    <th class="sort col-md-3" data-sort="open_spots">Rem<i class="fa fa-sort"></i></th>
+					<th colspan="2">
+			          <input type="text" class="search" placeholder="Search" />
+			        </th>
 			</thead>
 			<tbody class="list">
 				<?php foreach($events as &$event) { ?>
 				<tr>
 					<td class="title"><?php echo $event->getTitle(); ?></td>
-					<td class="date"><?php echo $event->getDate()->format('m-d'); ?></td>
+					<td class="date2"><?php echo $event->getDate()->format('m-d'); ?></td>
 					<td class="start_time"><?php echo $event->getStartTime()->format('H:i'); ?></td>
 					<td class="end_time"><?php echo $event->getEndTime()->format('H:i'); ?></td>
 					<td class="location"><?php echo $event->getLocation(); ?></td>
+                    <td class="attending"><?php echo $event->countInterests(); ?></td>
 					<td class="max_attendance"><?php echo $event->getMaxAttendance(); ?></td>
 					<td class="open_spots"><?php echo $event->getMaxAttendance() - $event->countInterests(); ?></td>
 					<td>
@@ -49,6 +52,7 @@
 	</div>
 	<script src="http://listjs.com/no-cdn/list.js"></script>
 	<script type="text/javascript">
+        
 		var options = {
 		  valueNames: [ 'title', 'date', 'start_time', 'end_time', 'location',
 		  	'attending', 'max_attendance', 'open_spots' ]
--- my_events.php
+++ my_events.php
@@ -23,6 +23,7 @@
 <html>
 <head>
 	<?php include_once 'basic_includes/sheets_and_scripts.php' ?>
+    <link href="//maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">
 	<title>Event List</title>
 </head>
 <body>
@@ -33,14 +34,14 @@
 		<div class="panel-body">
 			<table class="table table-hover">
 				<thead>
-					<th class="sort col-md-3" data-sort="title">Title</th>
-					<th class="sort col-md-1" data-sort="date">Date</th>
-					<th class="sort col-md-1" data-sort="start_time">Start Time</th>
-					<th class="sort col-md-1" data-sort="end_time">End Time</th>
-					<th class="sort col-md-2" data-sort="location">Location</th>
-					<th class="sort col-md-1" data-sort="attending">Attending</th>
-					<th class="sort col-md-1" data-sort="max_attendance">Max Attendance</th>
-					<th class="sort col-md-1" data-sort="open_spots">Open Spots</th>
+					<th class="sort col-md-4" data-sort="title">Title<i class="fa fa-sort"></i></th>
+					<th class="sort col-md-1" data-sort="date">Date<i class="fa fa-sort"></i></th>
+					<th class="sort col-md-1" data-sort="start_time">Start Time<i class="fa fa-sort"></i></th>
+					<th class="sort col-md-1" data-sort="end_time">End Time<i class="fa fa-sort"></i></i></th>
+					<th class="sort col-md-2" data-sort="location">Location<i class="fa fa-sort"></i></th>
+					<th class="sort col-md-1" data-sort="attending">Act<i class="fa fa-sort"></i></th>
+					<th class="sort col-md-1" data-sort="max_attendance">Cap<i class="fa fa-sort"></i></th>
+                    <th class="sort col-md-3" data-sort="open_spots">Rem<i class="fa fa-sort"></i></th>
 					<th colspan="2">
 			          <input type="text" class="search" placeholder="Search" />
 			        </th>
@@ -49,15 +50,15 @@
 					<?php foreach($my_events as &$event) { ?>
 					<tr>
 						<td class="title"><?php echo $event->getTitle(); ?></td>
-						<td class="date"><?php echo $event->getDate()->format('m-d'); ?></td>
+						<td class="date2"><?php echo $event->getDate()->format('m-d'); ?></td>
 						<td class="start_time"><?php echo $event->getStartTime()->format('H:i'); ?></td>
 						<td class="end_time"><?php echo $event->getEndTime()->format('H:i'); ?></td>
 						<td class="location"><?php echo $event->getLocation(); ?></td>
 						<td class="attending"><?php echo $event->countInterests(); ?></td>
 						<td class="max_attendance"><?php echo $event->getMaxAttendance(); ?></td>
-						<td class="open_spots"><?php echo $event->getMaxAttendance() - $event->countInterests(); ?></td>
+                        <td class="open_spots"><?php echo $event->getMaxAttendance() - $event->countInterests(); ?></td>
 						<td>
-							<a href="details.php?event_id=<?php echo $event->getPrimaryKey() ?>" >
+							<a href="events/details.php?event_id=<?php echo $event->getPrimaryKey() ?>" >
 								<button type="button" class="btn btn-info btn-sm">View</button>
 							</a>
 						</td>
@@ -72,14 +73,14 @@
 		<div class="panel-body">
 			<table class="table table-hover">
 				<thead>
-					<th class="sort col-md-3" data-sort="title">Title</th>
-					<th class="sort col-md-1" data-sort="date">Date</th>
-					<th class="sort col-md-1" data-sort="start_time">Start Time</th>
-					<th class="sort col-md-1" data-sort="end_time">End Time</th>
-					<th class="sort col-md-2" data-sort="location">Location</th>
-					<th class="sort col-md-1" data-sort="attending">Attending</th>
-					<th class="sort col-md-1" data-sort="max_attendance">Max Attendance</th>
-					<th class="sort col-md-1" data-sort="open_spots">Open Spots</th>
+					<th class="sort col-md-4" data-sort="title">Title<i class="fa fa-sort"></i></th>
+					<th class="sort col-md-1" data-sort="date">Date<i class="fa fa-sort"></i></th>
+					<th class="sort col-md-1" data-sort="start_time">Start Time<i class="fa fa-sort"></i></th>
+					<th class="sort col-md-1" data-sort="end_time">End Time<i class="fa fa-sort"></i></i></th>
+					<th class="sort col-md-2" data-sort="location">Location<i class="fa fa-sort"></i></th>
+					<th class="sort col-md-1" data-sort="attending">Act<i class="fa fa-sort"></i></th>
+					<th class="sort col-md-1" data-sort="max_attendance">Cap<i class="fa fa-sort"></i></th>
+                    <th class="sort col-md-3" data-sort="open_spots">Rem<i class="fa fa-sort"></i></th>
 					<th colspan="2">
 			          <input type="text" class="search" placeholder="Search" />
 			        </th>
@@ -96,10 +97,10 @@
 						<td class="max_attendance"><?php echo $event->getMaxAttendance(); ?></td>
 						<td class="open_spots"><?php echo $event->getMaxAttendance() - $event->countInterests(); ?></td>
 						<td>
-							<a href="details.php?event_id=<?php echo $event->getPrimaryKey() ?>" >
+							<a href="events/details.php?event_id=<?php echo $event->getPrimaryKey() ?>" >
 								<button type="button" class="btn btn-info btn-sm">View</button>
-							</a>
-						</td>
+		                    </a>
+		                </td>
 					</tr>
 					<?php } unset($event); ?>
 				</tbody>
